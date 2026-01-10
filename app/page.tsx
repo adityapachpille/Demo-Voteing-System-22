10191589a9c386878b550c4b62f52720cb68d3f6
adityapachpille <158599059+adityapachpille@users.noreply.github.com>
2026-01-10 16:45:08 +0530

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -82,7 +82,7 @@ const SoundCheck = () => {
 
           <tbody>
             {[...Array(10)].map((_, index) => (
-              <tr key={index} className="dark:bg-gray-800">
+              <tr key={index}>
                 <td className="border-2 border-gray-400 text-center font-bold text-sm">
                   {toMarathi(index + 1)}
                 </td>
